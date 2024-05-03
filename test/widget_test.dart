18fbddc6ff79ee08a71e6dfed37f8160c86cde67
Adeeb Abubacker <148857537+adeebca@users.noreply.github.com>
2024-05-03 21:18:11 +0530

    Made some changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:carrimen_app/main.dart';
+import 'package:yes_loyality/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
